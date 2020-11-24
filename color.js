7e4404406a14af4d134e8d98c1665243f85e7c2c
Y-Boyy <63579113+Y-Boyy@users.noreply.github.com>
2020-11-25 02:02:02 +0900

    My first Web

---
+++ color.js
@@ -0,0 +1,40 @@
+var Body={
+  setBackgroundColor:function(color){
+    //document.querySelector('body').style.backgroundColor=color;
+    $('body').css('backgroundColor',color);
+  },
+  setColor:function(color){
+    //document.querySelector('body').style.color=color;
+    $('body').css('color',color);
+  }
+}
+
+var Links={
+    setColor:function(color){
+
+    //var alist = document.querySelectorAll('a');
+    //var i=0;
+    //while(i<alist.length){
+    //  alist[i].style.color = color;
+    //  i+=1
+    $('a').css('color',color);
+
+    }
+  }
+
+  function nightDayHandler(self){
+  if(self.value === 'night'){
+
+    Body.setBackgroundColor('black');
+    Body.setColor('white');
+    self.value='day';
+
+    Links.setColor('white');
+  }else{
+    Body.setBackgroundColor('white');
+    Body.setColor('black');
+    self.value='night';
+
+    Links.setColor('black');
+  }
+}
